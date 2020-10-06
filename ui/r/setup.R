99a7d74ac00a58833693d6178a359da3aec439b8
Marcus Young <m.a.young@soton.ac.uk>
2020-10-06 14:17:32 +0100

    Build sdft R package from release not master

--- ui/r/setup.R
+++ ui/r/setup.R
@@ -1,6 +1,4 @@
 setwd("/tmp")
 install.packages(c("dplyr", "tidyr", "foreach", "RPostgres", "magrittr", "futile.logger", "stringr", "readr", "DBI", "doParallel", "keyring", "checkmate", "rlang"), repos = "https://cran.rstudio.com/", Ncpus = 4)
-unzip("master.zip", exdir="/tmp")
-file.rename("sdft-master", "sdft")
 system("R CMD INSTALL --build sdft")
 install.packages(list.files(path = "/tmp", pattern="sdft_"), repos = NULL)

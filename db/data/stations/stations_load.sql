70905cf73fd3f1647bc57d2fc8602b368ff6aca5
Marcus Young <m.a.young@soton.ac.uk>
2021-07-06 15:51:46 +0100

    Correct error with create table code

--- db/data/stations/stations_load.sql
+++ db/data/stations/stations_load.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c788ae19a4eb8b15fa3dff719f4a82b267220866bf2b1a9c5b94a69a7543bb4
+oid sha256:35c4f25397fd263a5847b76ac4963af69813af27dacba1a0bc5c5cba2e8f6347
 size 275091805
